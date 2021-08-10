9128e8e260b2d6fc70d32d09c5446971db752be1
Pavanjatla <53556145+Pavanjatla@users.noreply.github.com>
2021-08-10 11:05:46 +0530

    local storage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const taskContainer=document.querySelector(".task_container")
+let globalTaskData=[];
 
 const addNewCard = () =>{
     const taskData={
@@ -8,6 +9,10 @@ const addNewCard = () =>{
         Type: document.getElementById("taskType").value,
         Description : document.getElementById("taskDesc").value,
     };
+
+    globalTaskData.push(taskData);
+
+    localStorage.setItem("taskyCA",JSON.stringify({ card : globalTaskData }));
 
 
     const newCard = ` <div id=${taskData.id}  class="col-md-6 col-lg-4 my-5">
@@ -42,6 +47,57 @@ const addNewCard = () =>{
    document.getElementById("imageUrl").value="";
    document.getElementById("taskType").value="";
    document.getElementById("taskDesc").value="";
+
+
+};
+
+const loadCards=() => {
+
+  // check local storage
+
+  const getData=localStorage.getItem("taskyCA");
+
+  //parse data if exist
+
+  if(!getData) return;
+
+  const taskCards = JSON.parse(getData);
+    
+  globalTaskData =taskCards.card;
+
+  globalTaskData.map((taskData) =>{
+
+    const newCard = ` <div id=${taskData.id}  class="col-md-6 col-lg-4 my-5">
+
+    <div class="card">
+      <div class="card-header gap-2 d-flex justify-content-end">
+        <button class="btn btn-outline-info">
+          <i class="fas fa-pencil-alt"></i>
+        </button>
+        <button class="btn btn-outline-danger">
+          <i class="fas fa-trash-alt"></i>
+        </button>
+        
+      </div>
+      <div class="card-body">
+        <img src=${taskData.Image} alt="image"
+        class="card-img">
+        <h5 class="card-title">${taskData.title}</h5>
+        <p class="card-text">${taskData.Description}</p>
+         <span class="badge bg-primary">${taskData.Type}</span>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-outline-primary">Open Task</button> 
+      </div>
+    </div>
+
+  </div>`;
+
+  taskContainer.insertAdjacentHTML("beforeend",newCard);
+
+
+  })
+  return;
 
 
 };
